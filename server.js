fae4a6a2ff006ed399cd219ae2d36f545cb5e8e9
neysa21 <neysa21@gmail.com>
2017-07-06 12:26:31 -0400

    error with mongoose

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-'use strict'
+'use strict';
 // importing dependencies
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var Comment = require('./model/comments');
 //creating instances
 var app = express();
 var router = express.Router();
+
+
 //setting up port to 3001
 var port = process.env.API_PORT || 3001;
 
+
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds043972.mlab.com:43972/reactapp');
 // configure the API to use bodyParser and look for
 //JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,6 +33,33 @@ app.use(function(req, res, next) {
 router.get('/', function(req, res) {
  res.json({ message: 'API Initialized!'});
 });
+//adding the /comments route to our /api router
+router.route('/comments')
+ //retrieve all comments from the database
+  .get(function(req, res) {
+   //looks at our Comment Schema
+     Comment.find(function(err, comments) {
+       if (err) {
+         res.send(err);
+       } else {
+         res.json(comments)
+       }
+   //responds with a json object of our database comments.
+      });
+ })
+ //post new comment to the database
+//  .post(function(req, res) {
+//  var comment = new Comment();
+//  //body parser lets us use the req.body
+//  comment.author = req.body.author;
+//  comment.text = req.body.text;
+// comment.save(function(err) {
+//  if (err)
+//  res.send(err);
+//  res.json({ message: 'Comment successfully added!' });
+//  });
+//  });
+
 //Use our router configuration when we call /api
 app.use('/api', router);
 //starts the server and listens for requests
